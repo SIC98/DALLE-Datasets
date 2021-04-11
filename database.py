0825458bdfa4bc400dc9af2ef532f27f1e5db0d4
SIC98 <shininchul98@gmail.com>
2021-04-11 23:25:34 +0900

    url length: 2000 -> 1000

--- database.py
+++ database.py
@@ -67,7 +67,7 @@ class MySQLAPI:
                   Column('title', String(1000), nullable=False),
                   Column('raw_image', LargeBinary, nullable=True),
                   Column('image', LargeBinary, nullable=True),
-                  Column('url', String(2000), nullable=True),
+                  Column('url', String(1000), nullable=True),
                   Column('caption', String(2000), nullable=True)),
 
             self.metadata.create_all()
@@ -106,7 +106,7 @@ class TableClass(Base):
     image = Column(LargeBinary)
     mediatype = Column(String(50))
     mime = Column(String(50))
-    url = Column(String(2000))
+    url = Column(String(1000))
     caption = Column(String(2000))
 
     def __repr__(self):
