e654396223926195ee72ce5e2147b1ef60246f7c
SHIN IN CHUL <63440799+inchul-owhen@users.noreply.github.com>
2021-04-12 16:41:16 +0900

    support argparse

--- update_table.py
+++ update_table.py
@@ -1,9 +1,17 @@
 import asyncio
+import argparse
 from database import MySQLAPI
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--bulk', type=int, default=800)
+    parser.add_argument('-s', '--start_idx', type=int, default=0)
+    parser.add_argument('-e', '--end_idx', type=int, default=2000)
+    parser.add_argument('-p', '--processes', type=int, default=4)
+    a = parser.parse_args()
+
     db = MySQLAPI()
-    asyncio.run(db.update_table(1000, start_idx=0, end_idx=1000, processes=16))
+    asyncio.run(db.update_table(a.bulk, start_idx=a.start_idx, end_idx=a.end_idx, processes=a.processes))
     db.commit()
     db.close()
